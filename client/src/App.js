57b7f20172c080c3c72b05d1d8df4c817eaebba2
Nate <jnhatch3@gmail.com>
2022-05-20 13:15:52 -0400

    imported header child component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
+import Header from "./Header";
+
 function App() {
-  return <h1>test</h1>;
+  return (
+    <div className="container">
+      {/* <h1>test</h1> */}
+      <Header />
+    </div>
+  );
 }
 
 export default App;
